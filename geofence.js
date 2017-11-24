1d1482a1addf98265435ab492fcb68c7d0e8633b
Kiriti_Sai <kiriti_sai@MacBook-Pro.local>
2017-11-24 15:48:03 +0530

    Enter/Exit rectified

--- geofence.js
+++ geofence.js
@@ -4,7 +4,11 @@ const geolib = require("geolib");
 var express = require('express');
  var app = express();
 
+var i = 0;
+var j = 0;
 var check;
+var checkbig;
+var test = false;
 var port = process.env.PORT || 3333;
 const url =
   "http://test.peelabus.com/WebService.asmx/LiveTracking";
@@ -14,49 +18,85 @@ https.get(url, res => {
   res.setEncoding("utf8");
   let body = "";
   res.on("data", data => {
-    console.log(body += data);
+    body += data
+    // console.log("body += data");
   });
   res.on("end", () => {
     body = JSON.parse(body);
-    console.log(body.Result[0]);
-    // console.log(
-    //   `City: ${body.Result[0].Busid} -`,
-    //   `Latitude: ${body.Result[0].LAT} -`,
-    //   `Longitude: ${body.Result[0].LONG}` );
+    // console.log(body.Result[0]);
       let lat = body.Result[0].LAT;
       let long = body.Result[0].LONG;
-      console.log(lat);
-      console.log(long);
-      // Output the circle
-      // console.log(circle(5));
-      // checks if 51.525, 7.4575 is within a radius of 5km from 51.5175, 7.4678
-      console.log(geolib.isPointInCircle(
-        {latitude: lat, longitude: long},
-        {latitude: 17.809605, longitude: 83.387557},
-        500
-      ));
       check = geolib.isPointInCircle(
         {latitude: lat, longitude: long},
         {latitude: 17.809605, longitude: 83.387557},
         500
       );
-      console.log("heyyy");
+      checkbig = geolib.isPointInCircle(
+        {latitude: lat, longitude: long},
+        {latitude: 17.809605, longitude: 83.387557},
+        1000
+      );
+      console.log("hey");
   });
 });
-}, 5*1000);
+if (check == true) {
+    if (i == 0) {
+      console.log("insideradius:" +check + " enterorexit: Enter" );
+  // res.json({insideradius: check, enterorexit: "Enter" });
+  test = false;
+  i++;
+  j=0;
+} else {
+  console.log("insideradius:" +check+ " enterorexit: no change still enter" );
+  // res.json({insideradius: check, enterorexit: "no change still enter" });
+}
+} else if (checkbig == true && test == false) {
+    if (j == 0) {
+    console.log("exitinggggg");
+    console.log("insideradius:" +check+ " enterorexit: Exit" );
+  // res.json({insideradius: check, enterorexit: "Exit" });
+  test = true;
+  j++;
+  i=0;
+  }
+ else {
+  console.log("insideradius:" +check+ " enterorexit: no change still exit" );
+  // res.json({insideradius: check, enterorexit: "no change still exit" });
+}
+} else {
+  console.log("insideradius: "+ check+ " enterorexit: yet to be known" );
+   // res.json({insideradius: check, enterorexit: "yet to be known" });
+}
+}, 2*1000);
 
 
  app.get('/', function(req, res) {
-     res.json({insideradius: check, enterorexit: "yet to be known" });
+   console.log(test);
+   if (check == true) {
+       if (i == 0) {
+     res.json({insideradius: check, enterorexit: "Enter" });
+     test = false;
+     i++;
+     j = 0;
+   } else {
+     res.json({insideradius: check, enterorexit: "no change still enter" });
+   }
+
+ } else if (checkbig == true && test == false) {
+       if (j == 0) {
+       console.log("exitinggggg");
+     res.json({insideradius: check, enterorexit: "Exit" });
+     test = true;
+     j++;
+     i =0;
+     }
+    else {
+     res.json({insideradius: check, enterorexit: "no change still exit" });
+   }
+   } else {
+      res.json({insideradius: check, enterorexit: "yet to be known" });
+   }
  });
  app.listen(port, function() {
      console.log('Our app is running on http://localhost:' + port);
  });
-// geo() {
-//   geolib.isPointInCircle(
-//     {latitude: 51.525, longitude: 7.4575},
-//     {latitude: 51.5175, longitude: 7.4678},
-//     5000
-//   );
-//
-// }
